5a3f74821b3ff00f063e8ed334982529b66ef354
MiguelViapiana <migvjung@gmail.com>
2024-11-03 15:05:22 -0300

    Update AdicionarExercicio.kt

--- app/src/main/java/com/example/projeto_app_academia/ui/screen/treino/AdicionarExercicio.kt
+++ app/src/main/java/com/example/projeto_app_academia/ui/screen/treino/AdicionarExercicio.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -26,7 +31,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -82,16 +92,59 @@ private fun ConteudoPrincipalAdicionar(
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Text("Selecione uma Categoria")
+        Text("Selecione uma Categoria",
+            style = MaterialTheme.typography.titleLarge,
+            color = Color(0xFF275367),
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center,
+        )
         LazyColumn {
 
             items(categorias) { categoria ->
-                Text(
-                    text = categoria.nome,
-                    modifier = Modifier.clickable {
 
+                Card(
+                    shape = RoundedCornerShape(16.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0x63275367)),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                        .clickable{}
+
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.weight(3f),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = categoria.nome,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White,
+                                style = TextStyle(
+                                    shadow = Shadow(
+                                        color = Color(0x8B000000),
+                                        offset = Offset(10f, 4f),
+                                        blurRadius = 5f
+                                    )
+                                )
+                            )
+                        }
                     }
-                )
+                }
+
+//                Text(
+//                    text = categoria.nome,
+//                    modifier = Modifier.clickable {
+//
+//                    }
+//                )
             }
         }
 //        Button(
